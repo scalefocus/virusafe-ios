e7c26bc4fc402b14fb69eb5f01afce6b302f32a7
Gandi Pirkov <gandi.pirkov@scalefocus.com>
2020-03-20 23:01:07 +0200

    Adding Vicinity Implementation for other iOS Devices

---
+++ COVID-19/3rdPartyLibs/Objective-C-Vicinity/Bluetooth/Extensions/UILabel+Ext.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Ext.h
+//  
+//
+//  Created by Ben Ford on 6/24/13.
+//  Copyright (c) 2013 Ben Ford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UILabel(Ext)
+- (void)extAddPlaceHolderText:(NSString *)placeHolderText;
+- (void)extRemovePlaceHolderText:(NSString *)placeHolderText;
+@end
