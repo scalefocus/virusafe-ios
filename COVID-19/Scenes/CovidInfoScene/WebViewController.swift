04618c646b6bc2afc506560a4e437d04bd910672
Alexander S. Petrov <aleksandar.spetrov@scalefocus.com>
2020-03-25 16:23:41 +0200

    Add WebView Screen

---
+++ COVID-19/Scenes/CovidInfoScene/WebViewController.swift
@@ -0,0 +1,87 @@
+//
+//  WebViewController.swift
+//  COVID-19
+//
+//  Created by Aleksandar Sergeev Petrov on 25.03.20.
+//  Copyright © 2020 Upnetix AD. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+enum Source {
+    case content
+    case notification(String)
+
+    var urlString: String {
+        switch self {
+        case .content:
+            return "https://www.apple.com" // TODO: Replace it with hardcoded link to content
+        case .notification(let string):
+            return string
+        }
+    }
+}
+
+class WebViewController: UIViewController {
+
+    private var webView: WKWebView {
+        return view as! WKWebView
+    }
+
+    // MARK: Lifecycle
+
+    override func loadView() {
+        let webView = WKWebView()
+        // ??? Configuration
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    // MARK: Load
+
+    func load(source: Source) {
+        guard let url = URL(string: source.urlString) else {
+            assertionFailure("Invalid url: \(source.urlString)")
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+
+    // MARK: Setup UI
+
+    private func setupUI() {
+        // Setup webview
+        //e.g. webView.allowsBackForwardNavigationGestures = true
+    }
+
+}
+
+// MARK: WKUIDelegate
+
+extension WebViewController: WKUIDelegate {
+    // Do something if needed
+}
+
+// MARK: WKNavigationDelegate
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        LoadingIndicatorManager.startActivityIndicator(.whiteLarge, in: view)
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        LoadingIndicatorManager.stopActivityIndicator(in: view)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        LoadingIndicatorManager.stopActivityIndicator(in: view)
+    }
+}
---
+++ COVID-19/Scenes/CovidInfoScene/WebViewControllerFactory.swift
@@ -0,0 +1,100 @@
+//
+//  WebViewControllerFactory.swift
+//  COVID-19
+//
+//  Created by Aleksandar Sergeev Petrov on 25.03.20.
+//  Copyright © 2020 Upnetix AD. All rights reserved.
+//
+
+import Foundation
+
+extension WebViewController {
+    static func show(with source: Source) {
+        // Create Controller
+        guard let webViewController = UIStoryboard(name: "WebView", bundle: nil)
+            .instantiateViewController(withIdentifier: "\(WebViewController.self)")
+            as? WebViewController else {
+                assertionFailure("WebViewController is not found")
+                return
+        }
+
+        if #available(iOS 13.0, *) {
+            webViewController.isModalInPresentation = true // available in IOS13
+        }
+
+        // Add it as root in navigation controller
+        let navigationController = UINavigationController(rootViewController: webViewController)
+
+        // Add Close Button
+        let leftBarButtonItem = UIBarButtonItem.init(title: "Назад", style: .done) { [weak navigationController] _ in
+             navigationController?.presentingViewController?.dismiss(animated: true, completion: nil)
+        }
+        webViewController.navigationItem.leftBarButtonItem = leftBarButtonItem
+
+        if #available(iOS 13.0, *) {
+            if var topController = UIApplication.shared.keyWindow?.rootViewController  {
+                while let presentedViewController = topController.presentedViewController {
+                    topController = presentedViewController
+                }
+                topController.present(navigationController, animated: true) {
+                    webViewController.load(source: source)
+                }
+            }
+        } else {
+            // Root for this window
+            let rootViewController = UIViewController()
+            rootViewController.view.backgroundColor = .clear
+
+            // Create New Window
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.rootViewController = rootViewController
+            window.windowLevel = UIWindow.Level.alert + 1  // Swift 3-4: UIWindowLevelAlert + 1
+            window.makeKeyAndVisible()
+
+            // present (a little headache)
+            rootViewController.present(navigationController, animated: true) {
+                webViewController.load(source: source)
+            }
+        }
+
+    }
+}
+
+// MARK: Helper - Add Closure to UIBarButtonItem
+
+extension UIBarButtonItem {
+    typealias UIBarButtonItemTargetClosure = (UIBarButtonItem) -> ()
+
+    private class UIBarButtonItemClosureWrapper: NSObject {
+        let closure: UIBarButtonItemTargetClosure
+        init(_ closure: @escaping UIBarButtonItemTargetClosure) {
+            self.closure = closure
+        }
+    }
+
+    private struct AssociatedKeys {
+        static var targetClosure = "targetClosure"
+    }
+
+    private var targetClosure: UIBarButtonItemTargetClosure? {
+        get {
+            guard let closureWrapper = objc_getAssociatedObject(self, &AssociatedKeys.targetClosure) as? UIBarButtonItemClosureWrapper else { return nil }
+            return closureWrapper.closure
+        }
+        set(newValue) {
+            guard let newValue = newValue else { return }
+            objc_setAssociatedObject(self, &AssociatedKeys.targetClosure, UIBarButtonItemClosureWrapper(newValue), objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    convenience init(title: String?, style: UIBarButtonItem.Style, closure: @escaping UIBarButtonItemTargetClosure) {
+        self.init(title: title, style: style, target: nil, action: nil)
+        targetClosure = closure
+        action = #selector(UIBarButtonItem.closureAction)
+    }
+
+    @objc func closureAction() {
+        guard let targetClosure = targetClosure else { return }
+        targetClosure(self)
+    }
+}
