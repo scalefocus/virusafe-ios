b0e76bca7d6ea1508b81bf52cb66e8b26dd76110
Alexander S. Petrov <aleksandar.spetrov@scalefocus.com>
2020-05-01 14:40:26 +0300

    Add Branch Name Validation Action

--- .github/scripts/validate-code-style.sh
+++ .github/scripts/validate-code-style.sh
@@ -2,9 +2,6 @@
 
 files=$(git diff --name-only origin/$BASE_BRANCH_NAME origin/$BRANCH_NAME | grep -e '.*\.swift$' | sed -e 'N;s/\n/ /')
 
-# debug
-echo $files
-
 if [ -z "$files" ]
 then
 	echo "No changes in *.swift files"
